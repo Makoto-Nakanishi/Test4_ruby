db5680b8ac1e2c03b78cd52417b11b554b90d81d
Makoto-Nakanishi <makotonton5@gmail.com>
2022-05-01 11:58:50 +0900

    submit

--- memo.rb
+++ memo.rb
@@ -5,20 +5,28 @@ puts "1(新規でメモを作成) 2(既存のメモ編集する)"
 memo_type = gets.to_s.chomp
 
 if memo_type == "1" 
-    puts "文字を入力してください"
+    puts "拡張子を除いたファイル名を入力してください"
+
+    memoTitle = gets.chomp
     
-    textNew = gets.chomp
+    puts "メモしたい内容を入力してください。Ctrl+Dで保存します"
+
+    textNew = $stdin.read
     
-    CSV.open('memo.csv',"w") do |csv|
+    CSV.open("#{memoTitle}.csv","w") do |csv|
         csv.puts ["#{textNew}"]
     end
 
 elsif memo_type == "2"
-    puts "文字を入力してください"
+    puts "拡張子を除いたファイル名を入力してください"
+
+    memoTitle = gets.chomp
     
-    textAdd = gets.chomp
+    puts "メモしたい内容を入力してください。Ctrl+Dで保存します"
     
-    CSV.open('memo.csv',"a") do |csv|
+    textAdd = $stdin.read
+    
+    CSV.open("#{memoTitle}.csv","a") do |csv|
         csv.puts ["#{textAdd}"]
     end
 else 
